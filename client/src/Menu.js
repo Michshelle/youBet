7229d529e64d2c2214bfc91114d548cf1017d51c
Michshelle <shingyi.sung@gmail.com>
2021-10-06 22:24:39 +0800

    solved tezbridge.request

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 
+
 //const mutezToTez = mutez =>
 //  Math.round((parseInt(mutez) / 1000000 + Number.EPSILON) * 100) / 100;
 
 const Menu = ({
   tezos,
+  tezbridge,
   ledgerInstance,
   userAddress,
   setBalance,
@@ -13,37 +15,69 @@ const Menu = ({
 }) => {
   const [isBurnt, setIsBurnt] = useState(false);
 
-  const bet = async (stake_amount,bool_bet) => {
-    try {
-      const op = await ledgerInstance.methods.bet(bool_bet).send({ amount: stake_amount, mutez: true });
-      await op.confirmation(30);
-      if (op.includedInBlock !== Infinity) {
-        const newBalance = await tezos.rpc.getBalance(userAddress);
-        setBalance(newBalance);
-        alert("Transaction is done!")
-      } else {
-        throw Error("Transation not included in block");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+//const bet = async (stake_amount,bool_bet) => {
+//  try {
+//    alert("0 is done!");
+//    const op = await ledgerInstance.methods.bet('False').send({ amount: 1 * 1000000,fee: 100000, gasLimit: 1000000, mutez: true });
+//    alert("1 is done!");
+//    await op.confirmation(30);
+//    alert("2 is done!");
+//    if (op.includedInBlock !== Infinity) {
+//      const newBalance = await tezos.rpc.getBalance(userAddress);
+//      setBalance(newBalance);
+//      alert("Transaction is done!")
+//    } else {
+//      throw Error("Transation not included in block");
+//    }
+//  } catch (error) {
+//    console.log(error);
+//  }
+//}
+
+const bet = async (stake_amount,bool_bet) => {
+   tezbridge.request({
+     method: 'inject_operations',
+     operations: [
+       {
+        "kind": "transaction",
+        "source": userAddress,
+        "amount": stake_amount*1000000,
+        "destination": ledgerInstance.address,
+        "parameters":
+        {
+          "entrypoint":"bet",
+          "value":
+          {
+            "prim":bool_bet
+          }
+        }
+       }
+     ]
+   })
+   .then(result => alert(JSON.stringify(result)))
+   .catch(error => alert(error.toString()))
+};
+
+
 
   const burn = async () => {
-    try {
-      const op = await ledgerInstance.methods.withdrawal(0).send({ amount: 0 });
-      await op.confirmation(30);
-      if (op.includedInBlock !== Infinity) {
-        const newBalance = await tezos.rpc.getBalance(userAddress);
-        setBalance(newBalance);
-        setIsBurnt(true);
-        alert("Withdrawal is done!")
-      } else {
-        console.log("Transaction is not included in the block");
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    tezbridge.request({
+      method: 'inject_operations',
+      operations: [
+        {
+          kind: 'transaction',
+          source: userAddress,
+          destination: ledgerInstance.address,
+          amount: 0,
+          parameters: {
+            "entrypoint": "withdrawal"
+          }
+        }
+      ]
+    })
+    .then(result => alert(JSON.stringify(result)))
+    .catch(error => alert(error.toString()))
+
   };
   return (
     <>
@@ -82,7 +116,7 @@ const Menu = ({
                   <span
                     className="action is-medium" style={{"marginLeft":"10%"}}
                     onClick={async () => {
-                      await bet(document.getElementById("amountOfStake").value,document.getElementById("trend").value)
+                      await bet(parseFloat(document.getElementById("amountOfStake").value),document.getElementById("trend").value)
                       }
                     }
                   >
